9b7705eac4f1ed74f2a883b05b2f8dc28c6afbff
LyMeller <ly_melvin@yahoo.com>
2026-02-27 13:37:29 -0800

    Update fancifymytext.js

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,9 +1,31 @@
 const btn = document.querySelector("button");
+const radfbtn = document.getElementById("fancyShmancyButton");
+const radbbtn = document.getElementById("boringBettyButton");
+const mbtn = document.getElementById("moo");
+const suffix = "-Moo";
 
-function helloWorld() {
-    alert("Hello, world!");
+function changeFontSize() {
+    document.getElementById("userInput").style.fontSize = "24px";
 }
 
-btn.addEventListener("click", helloWorld);
+function changeFontStyle() {
+    document.getElementById("userInput").style.fontWeight = "bold";
+    document.getElementById("userInput").style.color = "blue"
+    document.getElementById("userInput").style.textDecorationLine = "underline";
+}
 
+function backToNormal() {
+    document.getElementById("userInput").style.fontWeight = "normal";
+    document.getElementById("userInput").style.color = "black"
+    document.getElementById("userInput").style.textDecorationLine = "none";
+}
 
+function moo() {
+    let text = document.getElementById("userInput").value.toUpperCase().split(".").join(".-Moo");
+    document.getElementById("userInput").value = text;
+}
+
+btn.addEventListener("click", changeFontSize);
+radfbtn.addEventListener("click", changeFontStyle);
+radbbtn.addEventListener("click", backToNormal);
+mbtn.addEventListener("click", moo);
